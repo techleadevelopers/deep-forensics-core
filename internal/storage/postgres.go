1ece5167cf30eefe3dedf68f30af08568551abc4
devopscomputer <pauloti.engenharia@gmail.com>
2026-07-05 10:35:07 -0300

    feat: update internal/storage/postgres.go

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -9,7 +9,7 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 
-	"github.com/verifood/verifood/internal/model"
+	"github.com/PixelAudit/PixelAudit/internal/model"
 )
 
 // Postgres é o wrapper em cima do pgxpool.
